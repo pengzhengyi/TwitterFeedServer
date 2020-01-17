dfe17cfbd006566b72decf5c569857e5141505d5
pengzhengyi <zhengyi_peng@brown.edu>
2020-01-17 18:40:51 -0500

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    name: 'TwitterFeedServer',
+    script: 'server.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+};
