7a1f05be1800bb0b4a43838c50fab749ef428b20
pengzhengyi <zhengyi_peng@brown.edu>
2020-01-17 19:15:03 -0500

    use multiple instances & save pid file in local directory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,14 @@ module.exports = {
     script: 'server.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    instances: 1,
+    instances: 0, // launch the maximum processes possible according to the numbers of CPUs (cluster mode)
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
 
     error_file: '.logs/error.log',
     out_file: '.logs/out.log',
+    pid_file: '.pid/app-pm_id.pid',
 
     env: {
       NODE_ENV: 'development'
