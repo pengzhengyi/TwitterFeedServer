b4649a6842df527a9ad18f057f06ab71d26220cf
pengzhengyi <zhengyi_peng@brown.edu>
2020-01-18 04:56:33 -0500

    Add handler for / route

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 // Express configuration
 app.set("port", 3000);
 
+app.get("/", function(req, res) {
+  res.status(200).send('<p>Welcome, you have reached Twitter Feed Server. Look at <a href="https://github.com/pengzhengyi/TwitterFeedServer#api-endpoints" target="_blank" rel="noopener noreferrer" title="API Doc"> API Documentation.</p>');
+});
+
 app.get("/streaming/start", startStreamingTweets);
 app.get("/streaming/stop", stopStreamingTweets);
 app.get("/streaming/stopall", stopAllStreamingTweets);
