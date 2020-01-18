afb03b4a364d173266035948d244a20e5459e553
pengzhengyi <zhengyi_peng@brown.edu>
2020-01-18 09:15:00 -0500

    setup npm run watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,10 @@ module.exports = {
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     instances: 0, // launch the maximum processes possible according to the numbers of CPUs (cluster mode)
     autorestart: true,
-    watch: false,
     max_memory_restart: '1G',
+
+    watch: false,
+    ignore_watch : ["node_modules", "[\/\\]\./", "example_responses", "logs", "README", "package.json", "package-lock.json", "vim-markdown-preview.html", "*config*"],
 
     error_file: '.logs/error.log',
     out_file: '.logs/out.log',
